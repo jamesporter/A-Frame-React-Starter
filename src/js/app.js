52c143e0c501d877a8f93a94786736fa521e8245
James Porter <james@amimetic.co.uk>
2016-06-18 17:45:16 +0100

    basic bar chart with selection on look implemented

--- src/js/app.js
+++ src/js/app.js
@@ -11,20 +11,66 @@ import Camera from './components/Camera';
 import Cursor from './components/Cursor';
 import Sky from './components/Sky';
 
-class BoilerplateScene extends React.Component {
+class SimpleVisualisationScene extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      color: 'red'
+      data: [1.0, 2.0, 2.1, 0.4, 1.2, 3.4, 2.5],
+      labels: ["A", "B", "C", "D", "E", "F", "G"],
+        selection: null
     }
   }
 
-  changeColor = () => {
-    const colors = ['red', 'orange', 'yellow', 'green', 'blue'];
-    this.setState({
-      color: colors[Math.floor(Math.random() * colors.length)],
-    });
-  };
+    setSelection(selection){
+        this.setState({selection})
+    }
+
+    clearSelection(){
+        this.setState({selection: null})
+    }
+
+    coordsToPosition(coords){
+        return `${coords.x} ${coords.y} ${coords.z}`
+    }
+    
+  renderValue(idx, d){
+      const length = this.state.data.length;
+
+      const coords = {
+          x: 15.0* idx/length - 7.5,
+          y: d/2 - 2,
+          z: -2
+      };
+
+      const textCoords = {
+          x: 15.0* idx/length - 7.5,
+          y: -3,
+          z: -2
+      };
+
+      const startCoords = {
+          x: 15.0* idx/length - 7.5,
+          y: -2,
+          z: -2
+      };
+
+      return (
+          [
+          <Entity geometry={`primitive: box; width:2; height: ${d}; depth: 0.1;`}
+                  material={ this.state.selection == idx? {color: "white"} :{color: "orange"}}
+                  position={this.coordsToPosition(coords)}
+                  onMouseEnter={() => this.setSelection(idx)}
+                  onMouseLeave={() => this.clearSelection(idx)}
+          >
+              <Animation attribute="scale" dur="5000"from ="1 0 1" to="1 1 1"/>
+              <Animation attribute="position" dur="5000" from ={this.coordsToPosition(startCoords)} to={this.coordsToPosition(coords)}/>
+          </Entity>,
+          <Entity text={"text: " + this.state.labels[idx]}
+                  material={ this.state.selection == idx? {color: "white"} :{color: "orange"}}
+                  position={this.coordsToPosition(textCoords)}/>
+        ]
+    )
+  }
 
   render () {
     return (
@@ -33,34 +79,14 @@ class BoilerplateScene extends React.Component {
 
         <Sky/>
 
+          {this.state.data.map((data, i) => this.renderValue(i, data))}
+
         <Entity light={{type: 'ambient', color: '#888'}}/>
         <Entity light={{type: 'directional', intensity: 0.5}} position={[-1, 1, 0]}/>
         <Entity light={{type: 'directional', intensity: 1}} position={[1, 1, 0]}/>
-
-          <Entity text="text: What's up?" material={{color: 'orange'}} position="-5 0 -5">
-              <Animation attribute="rotation" dur="5000" repeat="indefinite" to="0 360 360"/>
-          </Entity>
-
-        <Entity geometry="primitive: box" material={{color: this.state.color}}
-                onClick={this.changeColor}
-                position="0 0 -5">
-          <Animation attribute="rotation" dur="5000" repeat="indefinite" to="0 360 360"/>
-        </Entity>
-
-          <Entity geometry="primitive: box" material={{color: this.state.color}}
-                  onMouseEnter={this.changeColor} onMouseLeave={this.changeColor}
-                  position="0 3 -5">
-              <Animation attribute="rotation" dur="5000" repeat="indefinite" to="360 360 0"/>
-          </Entity>
-
-          <Entity geometry="primitive: box" material={{color: this.state.color}}
-                  onClick={this.changeColor}
-                  position="3 0 -5">
-              <Animation attribute="rotation" dur="5000" repeat="indefinite" to="0 360 360"/>
-          </Entity>
       </Scene>
     );
   }
 }
 
-ReactDOM.render(<BoilerplateScene/>, document.querySelector('.scene-container'));
+ReactDOM.render(<SimpleVisualisationScene/>, document.querySelector('.scene-container'));
